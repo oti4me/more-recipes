4529f34b14d6b096be3a321bd8f0226068142b10
Henry Otighe <oti4me@gmail.com>
2017-11-09 15:33:44 +0100

    refactor server

--- server/mockapi/data.js
+++ server/mockapi/data.js
@@ -13,7 +13,9 @@ export default {
 			id : 1,
 			title : "Ginger Rice",
 			description : "he extension uses the ESLint library installed in the opened workspace folder. If the folder doesn't provide one the extension looks for a global install version. If you haven't installed ESLint either locally or globally do so by running ",
-			img : "Henry Otighe",
+			direction : "he extension uses the ESLint library installed in the opened workspace folder. If the folder doesn't provide one the extension looks for a global install version. If you haven't installed ESLint either locally or globally do so by running ",
+			ingredients : "he extension uses the ESLint library installed in the opened workspace folder. If the folder doesn't provide one the extension looks for a global install version. If you haven't installed ESLint either locally or globally do so by running ",
+			img : "/images/2.jpg",
 			userId : 1,
 			upvotes : 0,
 			downvotes : 0,
@@ -23,7 +25,9 @@ export default {
 			id : 2,
 			title : "Diced Pineaples",
 			description : "he extension uses the ESLint library installed in the opened workspace folder. If the folder doesn't provide one the extension looks for a global install version. If you haven't installed ESLint either locally or globally do so by running ",
-			img : "Henry Otighe",
+			direction : "he extension uses the ESLint library installed in the opened workspace folder. If the folder doesn't provide one the extension looks for a global install version. If you haven't installed ESLint either locally or globally do so by running ",
+			ingredients : "he extension uses the ESLint library installed in the opened workspace folder. If the folder doesn't provide one the extension looks for a global install version. If you haven't installed ESLint either locally or globally do so by running ",
+			img : "/images/1.jpg",
 			userId : 2,
 			upvotes : 0,
 			downvotes : 0,
@@ -33,7 +37,45 @@ export default {
 			id : 3,
 			title : "Bacon source",
 			description : "he extension uses the ESLint library installed in the opened workspace folder. If the folder doesn't provide one the extension looks for a global install version. If you haven't installed ESLint either locally or globally do so by running ",
-			img : "Henry Otighe",
+			direction : "he extension uses the ESLint library installed in the opened workspace folder. If the folder doesn't provide one the extension looks for a global install version. If you haven't installed ESLint either locally or globally do so by running ",
+			ingredients : "he extension uses the ESLint library installed in the opened workspace folder. If the folder doesn't provide one the extension looks for a global install version. If you haven't installed ESLint either locally or globally do so by running ",
+			img : "/images/3.jpg",
+			userId : 1,
+			upvotes : 0,
+			downvotes : 0,
+			reviews: []
+		},
+		{
+			id : 4,
+			title : "Icecream sandwich",
+			description : "he extension uses the ESLint library installed in the opened workspace folder. If the folder doesn't provide one the extension looks for a global install version. If you haven't installed ESLint either locally or globally do so by running ",
+			direction : "he extension uses the ESLint library installed in the opened workspace folder. If the folder doesn't provide one the extension looks for a global install version. If you haven't installed ESLint either locally or globally do so by running ",
+			ingredients : "he extension uses the ESLint library installed in the opened workspace folder. If the folder doesn't provide one the extension looks for a global install version. If you haven't installed ESLint either locally or globally do so by running ",
+			img : "/images/3.jpg",
+			userId : 1,
+			upvotes : 0,
+			downvotes : 0,
+			reviews: []
+		},
+		{
+			id : 5,
+			title : "Bread and eggs",
+			description : "he extension uses the ESLint library installed in the opened workspace folder. If the folder doesn't provide one the extension looks for a global install version. If you haven't installed ESLint either locally or globally do so by running ",
+			direction : "he extension uses the ESLint library installed in the opened workspace folder. If the folder doesn't provide one the extension looks for a global install version. If you haven't installed ESLint either locally or globally do so by running ",
+			ingredients : "he extension uses the ESLint library installed in the opened workspace folder. If the folder doesn't provide one the extension looks for a global install version. If you haven't installed ESLint either locally or globally do so by running ",
+			img : "/images/5.jpg",
+			userId : 2,
+			upvotes : 0,
+			downvotes : 0,
+			reviews: []
+		},
+		{
+			id : 6,
+			title : "Beans and Yam",
+			description : "he extension uses the ESLint library installed in the opened workspace folder. If the folder doesn't provide one the extension looks for a global install version. If you haven't installed ESLint either locally or globally do so by running ",
+			direction : "he extension uses the ESLint library installed in the opened workspace folder. If the folder doesn't provide one the extension looks for a global install version. If you haven't installed ESLint either locally or globally do so by running ",
+			ingredients : "he extension uses the ESLint library installed in the opened workspace folder. If the folder doesn't provide one the extension looks for a global install version. If you haven't installed ESLint either locally or globally do so by running ",
+			img : "/images/6.jpg",
 			userId : 1,
 			upvotes : 0,
 			downvotes : 0,
@@ -44,14 +86,14 @@ export default {
 	"reviews" : [
 		{
 			id : 1,
-			description : "he extension uses the ESLint library installed in the opened workspace folder. If the folder doesn't provide one the extension looks for a global install version. If you haven't installed ESLint either locally or globally do so by running ",
+			comment : "he extension uses the ESLint library installed in the opened workspace folder. If the folder doesn't provide one the extension looks for a global install version. If you haven't installed ESLint either locally or globally do so by running ",
 			recipeId : 1,
 			userId : 2
 			
 		},
 		{
 			id : 2,
-			description : "he extension uses the ESLint library installed in the opened workspace folder. If the folder doesn't provide one the extension looks for a global install version. If you haven't installed ESLint either locally or globally do so by running ",
+			comment : "he extension uses the ESLint library installed in the opened workspace folder. If the folder doesn't provide one the extension looks for a global install version. If you haven't installed ESLint either locally or globally do so by running ",
 			recipeId : 2,
 			userId : 2
 			
