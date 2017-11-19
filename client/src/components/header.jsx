da36c00c53c897ba4bfed518922eb360d687c543
Henry Otighe <oti4me@gmailc.om>
2017-11-19 16:19:46 +0100

    add dropdown to the header

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -9,11 +9,28 @@ class Header extends React.Component {
     super(props);
   }
 
+  componentDidMount(){
+    $('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrainWidth: false, 
+      hover: true, 
+      gutter: 0, 
+      belowOrigin: false, 
+      alignment: 'left', 
+      stopPropagation: false
+    }
+  );
+  }
+
   handleSignOut(){
     this.props.signOut(this.props.history);
   }
 
   topMenu() {
+    const style1 = {
+      marginTop: '65px'
+    }
     console.log(this.props.state.loggedIn)
     if(!this.props.state.loggedIn){
       return (
@@ -44,13 +61,22 @@ class Header extends React.Component {
               <a href="/home">Home</a>
             </li>
             <li>
-              <a href="/profile">User Account</a>
+              <a className="dropdown-button" href="#" data-activates='dropdown1'>User Account</a>
             </li>
             <li>
               <a href="" onClick={this.handleSignOut.bind(this)} >Logout</a>
             </li>
           </ul>
         </div>
+        <ul id='dropdown1' className='dropdown-content' style={style1}>
+          <li><a href="/profile">Profile</a></li>
+          <li className="divider"></li>
+          <li><a href="addrecipe">Add Recipe</a></li>
+          <li className="divider"></li>
+          <li><a href="myrecipes">My Recipes</a></li>
+          <li className="divider"></li>
+          <li><a href="favourites">Favourite Recipes</a></li>
+        </ul>
       </nav>
     );
     }
